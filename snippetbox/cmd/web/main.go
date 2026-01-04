d41a3402c3e9b205484af3b530c3885f1f3ba8f0
Porcelain_ <ai.red.porcelain@gmail.com>
2026-01-04 20:58:40 +0800

    using commadn line to configure port

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.HandleFunc("GET /{$}", home)
@@ -14,7 +18,7 @@ func main() {
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	mux.Handle("/house", &house{})
-	log.Print("starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("starting server on %s ", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
