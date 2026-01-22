70561854aae0423134322a6ed2aae281bfd2a597
Porcelain_ <ai.red.porcelain@gmail.com>
2026-01-22 14:24:12 +0800

    feat: Add snippet creation form and refactor middleware chaining with alice.

--- snippetbox/cmd/web/routes.go
+++ snippetbox/cmd/web/routes.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"github.com/justinas/alice"
+	"net/http"
+)
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
@@ -12,5 +15,6 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	return app.recoverPanic(app.logRequest(commonHeader(mux)))
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeader)
+	return standard.Then(mux)
 }
