0a6a98cbb6b1ae93be992a9be93b4a220e551747
Porcelain_ <ai.red.porcelain@gmail.com>
2026-01-24 21:53:57 +0800

    feat: Add idle, read, and write timeouts to the HTTP server configuration.

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -59,10 +59,13 @@ func main() {
 	}
 
 	srv := &http.Server{
-		Addr:      *addr,
-		Handler:   app.routes(),
-		ErrorLog:  slog.NewLogLogger(logger.Handler(), slog.LevelError),
-		TLSConfig: tlsConfig,
+		Addr:         *addr,
+		Handler:      app.routes(),
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  60 * time.Second,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	logger.Info("starting server", "addr", srv.Addr)
