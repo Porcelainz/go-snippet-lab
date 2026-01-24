557d45c419ce667037e0144cfbd5742157b868e4
Porcelain_ <ai.red.porcelain@gmail.com>
2026-01-24 21:33:39 +0800

    feat: Configure http.Server to use slog for error logging and log the actual server address.

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -54,11 +54,12 @@ func main() {
 	}
 
 	srv := &http.Server{
-		Addr:    *addr,
-		Handler: app.routes(),
+		Addr:     *addr,
+		Handler:  app.routes(),
+		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
 	}
 
-	logger.Info("starting server", "addr", *addr)
+	logger.Info("starting server", "addr", srv.Addr)
 	err = srv.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
